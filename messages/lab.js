111be119e4f920c051c2347e31fcfa769624aef4
Kazi Tanzim Islam <kazi.islam@tufts.edu>
2016-03-11 19:50:02 -0500

    Added JS code to display messages

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,23 @@
 // Your JavaScript goes here...
+function parse() {
+
+	request = new XMLHttpRequest();
+	request.open("GET", "data.json", true);
+	request.onreadystatechange = callme;
+	request.send(null);
+};
+
+function callme() {
+	if (request.readyState ==4 && request.status == 200){
+		data = request.responseText;	
+		message = JSON.parse(data);	
+		elem = document.getElementById("messages");
+		for (count = 0; count < message.length; count++) {
+			elem.innerHTML += "<p>" + message[count]["content"] + " " + message[count]["username"] + "</p>"; 
+			
+		}
+	}
+	
+};
+
+
